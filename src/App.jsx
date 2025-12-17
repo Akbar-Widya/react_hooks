53f5d821417f7f1068f4e352de2ecb6fc98bf92c
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-17 22:57:00 +0700

    topic: use effect: connecting to external system: example one - listening to a global browser event

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,32 @@
 import { useEffect, useState } from "react";
-import { createConnection } from "./chat";
-
-const ChatRoom = ({ roomId }) => {
-   const [serverUrl, setServerUrl] = useState("http://localhost:3000");
-
-   useEffect(() => {
-      const connection = createConnection(serverUrl, roomId);
-      connection.connect();
-      console.log(`Connected to room: ${roomId} at ${serverUrl}`);
-      // a cleanup function
-      // one of its usage is to remove old dependencies, when the dependencies change
-      return () => {
-         connection.disconnect();
-      };
-   }, [serverUrl, roomId]);
-   return (
-      <>
-         <label>
-            Server URL:{' '}
-            <input
-               value={serverUrl}
-               onChange={e => setServerUrl(e.target.value)}
-            />
-         </label>
-         <h1>Welcome to the {roomId} room!</h1>
-      </>
-   );
-};
 
 const App = () => {
-   const [roomId, setRoomId] = useState("general");
-   const [show, setShow] = useState(false);
-   return (
-      <>
-         <label>
-            Choose the chat room:{' '}
-            <select
-               value={roomId}
-               onChange={e => setRoomId(e.target.value)}
-            >
-               <option value="general">general</option>
-               <option value="travel">travel</option>
-               <option value="music">music</option>
-            </select>
-         </label>
-         <button onClick={() => setShow(!show)}>
-            {show ? 'Close chat' : 'Open chat'}
-         </button>
-         {show && <hr />}
-         {show && <ChatRoom roomId={roomId} />}
-      </>
-   );
-};
-export default App;
+   const [position, setPosition] = useState({ x: 0, y: 0 });
+
+   useEffect(() => {
+      const handleMove = (e) => {
+         setPosition({x: e.clientX, y: e.clientY})
+      }
+      // This window event seen by React as an external system
+      window.addEventListener('pointermove', handleMove)
+      return () => {
+         window.removeEventListener('pointermove', handleMove)
+      }
+   }, [])
+  return (
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'pink',
+      borderRadius: '50%',
+      opacity: 0.6,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+    }}
+    />
+  )
+}
+export default App
