3298fe476a6575379c5cba2eacdb553fbe26ae42
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-17 23:41:21 +0700

    topic: use effect: controlling a non-react widget: example: Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
 import { useState } from "react";
-import { useChatRoom } from "./useChatRoom";
-
-const ChatRoom = ({ roomId }) => {
-   const [serverUrl, setServerUrl] = useState("http://localhost:3000");
-
-   useChatRoom({
-      roomId: roomId,
-      serverUrl: serverUrl,
-   });
-   return (
-      <>
-         <label>
-            Server URL:{" "}
-            <input
-               value={serverUrl}
-               onChange={(e) => setServerUrl(e.target.value)}
-            />
-         </label>
-         <h1>Welcome to the {roomId} room!</h1>
-      </>
-   );
-};
+import Map from "./Map";
 
 const App = () => {
-   const [roomId, setRoomId] = useState("general");
-   const [show, setShow] = useState(false);
+   const [zoomLevel, setZoomLevel] = useState(0);
    return (
       <>
-         <label>
-            Choose the chat room:{" "}
-            <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
-               <option value="general">general</option>
-               <option value="travel">travel</option>
-               <option value="music">music</option>
-            </select>
-         </label>
-         <button onClick={() => setShow(!show)}>
-            {show ? "Close chat" : "Open chat"}
-         </button>
-         {show && <hr />}
-         {show && <ChatRoom roomId={roomId} />}
+         <p>Zoom level: {zoomLevel}</p>
+         <button onClick={() => setZoomLevel(zoomLevel + 1)}>+</button>
+         <button onClick={() => setZoomLevel(zoomLevel - 1)}>-</button>
+         <hr />
+         <Map zoomLevel={zoomLevel} />
       </>
    );
 };
---
+++ src/Map.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react"
+import { MapWidget } from "./map-widget"
+
+const Map = ({zoomLevel}) => {
+   const containerRef = useRef(null)
+   const mapRef = useRef(null)
+
+   useEffect(() => {
+      if (mapRef.current === null) {
+         mapRef.current = new MapWidget(containerRef.current)
+      }
+
+      const map = mapRef.current
+      map.setZoom(zoomLevel)
+   }, [zoomLevel])
+  return (
+    <div 
+      style={{ width: 400, height: 400 }}
+      ref={containerRef}
+    />
+  )
+}
+export default Map
