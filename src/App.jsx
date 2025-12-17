6dc21f23fb0a930f2e2e91306a27fb19c795cb2b
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-17 23:26:12 +0700

    topic: use effect: wrapping effects in custom hooks: example

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import { useState } from "react";
-import ModalDialog from "./ModalDialog";
+import { useChatRoom } from "./useChatRoom";
+
+const ChatRoom = ({ roomId }) => {
+   const [serverUrl, setServerUrl] = useState("http://localhost:3000");
+
+   useChatRoom({
+      roomId: roomId,
+      serverUrl: serverUrl,
+   });
+   return (
+      <>
+         <label>
+            Server URL:{" "}
+            <input
+               value={serverUrl}
+               onChange={(e) => setServerUrl(e.target.value)}
+            />
+         </label>
+         <h1>Welcome to the {roomId} room!</h1>
+      </>
+   );
+};
 
 const App = () => {
+   const [roomId, setRoomId] = useState("general");
    const [show, setShow] = useState(false);
    return (
       <>
-         <button onClick={() => setShow(true)}>Open dialog</button>
-         <ModalDialog isOpen={show}>
-            Hello there!
-            <br />
-            <button
-               onClick={() => {
-                  setShow(false);
-               }}
-            >
-               Close
-            </button>
-         </ModalDialog>
+         <label>
+            Choose the chat room:{" "}
+            <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+               <option value="general">general</option>
+               <option value="travel">travel</option>
+               <option value="music">music</option>
+            </select>
+         </label>
+         <button onClick={() => setShow(!show)}>
+            {show ? "Close chat" : "Open chat"}
+         </button>
+         {show && <hr />}
+         {show && <ChatRoom roomId={roomId} />}
       </>
    );
 };
